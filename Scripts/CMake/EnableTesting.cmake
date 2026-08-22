f50639803497907cf95ba9fb5f170458f430450f
Dylan Hollemaert <hollemaertdylan@gmail.com>
2026-08-22 22:33:48 +0200

    clean-up
    Signed-off-by: Dylan Hollemaert <hollemaertdylan@gmail.com>

--- Scripts/CMake/EnableTesting.cmake
+++ Scripts/CMake/EnableTesting.cmake
@@ -2,12 +2,14 @@
 
 include(FetchContent)
 FetchContent_Declare(
-    googletest
-    GIT_REPOSITORY https://github.com/google/googletest.git
-    GIT_TAG        v1.18.0
+        googletest
+        GIT_REPOSITORY https://github.com/google/googletest.git
+        GIT_TAG        v1.18.0
 )
 
 # See: https://github.com/google/googletest/blob/main/docs/quickstart-cmake.md
 set(gtest_force_shared_crt ON CACHE BOOL "" FORCE)
 
 FetchContent_MakeAvailable(googletest)
+
+message(STATUS "Googletest: Version 1.18.0")
--- Source/Editor/Application/Private/Application.cpp
+++ Source/Editor/Application/Private/Application.cpp
@@ -9,7 +9,6 @@ namespace Nexus {
     void Application::run() {
         Logger logger;
         logger.LogInfo(std::format("Hello {}", Config::engineName));
-        logger.LogTrace("Hey");
     }
 
 } // namespace Nexus
